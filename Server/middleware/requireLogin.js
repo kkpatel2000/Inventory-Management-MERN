8081097391b250f11599178304fb7f5ed30db714
kkpatel2000 <66170174+kkpatel2000@users.noreply.github.com>
2020-09-25 21:46:23 +0530

    Create requireLogin.js

---
+++ Server/middleware/requireLogin.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+const mongoose =require('mongoose')
+const User = mongoose.model("User")
+
+module.exports = (req,res,next)=>{
+    const {authorization} = req.headers
+    if(!authorization){
+        return res.json({error:"you must be login"})
+    }
+   const token = authorization.replace("Bearer ","")
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+        if(err){
+        return res.status(401).json({error:"you must be loged in"})
+        }
+        const {_id} = payload
+        User.findById(_id).then(userdata=>{
+            req.user = userdata
+            next()
+
+        })
+    })
+}
